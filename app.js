1a771e36d573a85e9aba334b6fbb411cd434b267
Hafizuddin <lapizjusoh@yahoo.com>
2022-09-13 18:08:15 +0800

    Assign div box id into an array

--- app.js
+++ app.js
@@ -15,24 +15,58 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById(`player2-name`).value = ``;
   })
 
-  let box00 = document.getElementById(`box00`);
-  let box01 = document.getElementById(`box01`);
-  let box02 = document.getElementById(`box02`);
-  let box10 = document.getElementById(`box10`);
-  let box11 = document.getElementById(`box11`);
-  let box12 = document.getElementById(`box12`);
-  let box20 = document.getElementById(`box20`);
-  let box21 = document.getElementById(`box21`);
-  let box22 = document.getElementById(`box22`);
 
   for(let i = 0; i < 3; i++) {
     for(let j = 0; j < 3; j++) {
-      document.getElementById(`box${i}${j}`).addEventListener(`click`, () => {
-        document.getElementById(`box${i}${j}`).style.backgroundColor = `red`;
-      })  
+
     }
   }
+
+  // let box00 = document.getElementById(`box00`);
+  // let box01 = document.getElementById(`box01`);
+  // let box02 = document.getElementById(`box02`);
+  // let box10 = document.getElementById(`box10`);
+  // let box11 = document.getElementById(`box11`);
+  // let box12 = document.getElementById(`box12`);
+  // let box20 = document.getElementById(`box20`);
+  // let box21 = document.getElementById(`box21`);
+  // let box22 = document.getElementById(`box22`);
+
+  let boxArr = [];
+
+  for(let i = 0; i < 3; i++) {
+    boxArr.push([]);
+    for(let j = 0; j < 3; j++) {
+      boxArr[i].push(document.getElementById(`box${i}${j}`))
+    }
+  }
+
+  /*
+  Assign the div box ID into arrays. Example:
+    document.getElementById(`box00`) = boxArr[0][0];
+    document.getElementById(`box01`) = boxArr[0][1];
+  */
+
+  for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+      boxArr[i][j].addEventListener(`click`, () => {
+        boxArr[i][j].style.backgroundColor = `red`;
+      })
+    }
+  }
+
+
+  // for(let i = 0; i < 3; i++) {
+  //   for(let j = 0; j < 3; j++) {
+  //     document.getElementById(`box${i}${j}`).addEventListener(`click`, () => {
+  //       document.getElementById(`box${i}${j}`).style.backgroundColor = `red`;
+  //     })  
+  //   }
+  // }
   
+  // if((box00.style.backgroundColor == 'red') && (box01.style.backgroundColor == 'red') && (box02.style.backgroundColor == 'red')) {
+  //   alert(`Congratulations! ${play1} has won the game!`)
+  // }
 
 
 })
