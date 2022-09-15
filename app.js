4bd9968ca0033a158bbac95e26bec3cbbdb6a1f8
Hafizuddin <lapizjusoh@yahoo.com>
2022-09-15 15:21:10 +0800

    Players now take turns marking the tiles

--- app.js
+++ app.js
@@ -1,65 +1,93 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  // Declare Players
+
+  class Player {
+    constructor (color) {
+      this.name = ``;
+      this.score = 0;
+      this.color = color
+    }
+
+    changePlayer()  {
+      if (this == play1) {
+        currentPlayer = play2;
+      } else if (this == play2) {
+        currentPlayer = play1;
+      }
+    }
+
+  }
+
+  const play1 = new Player('red');
+  const play2 = new Player('blue')
+ 
+
+
   // display player names when they input their names and click the submit button
 
-  let play1 = `Player 1`;
-  let play2 = `Player 2`;
-
   document.getElementById(`submitPlay1Name`).addEventListener('click', () => {
-    play1 = document.getElementById(`player1-name`).value;
-    document.getElementById(`displayPlay1Name`).innerText = ` Welcome back, ${play1}`;
-    document.getElementById(`player1-name`).value = ``;
+    play1.name = document.getElementById(`player1-name`).value;
+    document.getElementById(`displayPlay1Name`).innerText = ` ${play1.name}`;
+    document.getElementById(`player1-name`).style.display = `none`;
+    document.getElementById(`submitPlay1Name`).style.display = `none`;
   })
   
   document.getElementById(`submitPlay2Name`).addEventListener('click', () => {
-    play2 = document.getElementById(`player2-name`).value;
-    document.getElementById(`displayPlay2Name`).innerText = ` Welcome back, ${play2}`;
-    document.getElementById(`player2-name`).value = ``;
+    play2.name = document.getElementById(`player2-name`).value;
+    document.getElementById(`displayPlay2Name`).innerText = ` Welcome back, ${play2.name}`;
+    document.getElementById(`player2-name`).style.display = `none`;
+    document.getElementById(`submitPlay2Name`).style.display = `none`;
   })
+
+  // check win condition
+
+  const checkMatchThree = () => {
+
+    for(let i =0; i < 3; i++) {
+      if(
+        ( (boxArr[i][0].style.backgroundColor == 'red') && (boxArr[i][1].style.backgroundColor == 'red') && (boxArr[i][2].style.backgroundColor == 'red') ) ||
+        ( (boxArr[0][i].style.backgroundColor == 'red') && (boxArr[1][i].style.backgroundColor == 'red') && (boxArr[2][i].style.backgroundColor == 'red') ) ||
+        ( (boxArr[0][0].style.backgroundColor == 'red') && (boxArr[1][1].style.backgroundColor == 'red') && (boxArr[2][2].style.backgroundColor == 'red'))
+      ) {
+        document.getElementById(`result`).innerHTML = `That's a match three! ${play1.name} won!`;
+        isWon = true;
+      }
+    }
+  }
+  
+  // setting variables
+
+  let isWon = false;
+  let currentPlayer = play1;
 
   /*
   Assign the div box ID into arrays. Example:
     boxArr[0][0] = document.getElementById(`box00`);
     boxArr[0][1] = document.getElementById(`box01`);
   */
+    let boxArr = [];
 
-  let boxArr = [];
-  let isWon = false;
-
-  for(let i = 0; i < 3; i++) {
-    boxArr.push([]);
-    for(let j = 0; j < 3; j++) {
-      boxArr[i].push(document.getElementById(`box${i}${j}`))
+    for(let i = 0; i < 3; i++) {
+      boxArr.push([]);
+      for(let j = 0; j < 3; j++) {
+        boxArr[i].push(document.getElementById(`box${i}${j}`))
+      }
     }
-  }
 
-  // on click, change box color
+  // on click and if nobody wins yet, change box color.
 
   for(let i = 0; i < 3; i++) {
     for(let j = 0; j < 3; j++) {
       boxArr[i][j].addEventListener(`click`, () => {
         if (!isWon) {
-        boxArr[i][j].style.backgroundColor = `red`;
-        checkMatchThree();
+          boxArr[i][j].style.backgroundColor = currentPlayer.color;
+          checkMatchThree();
+          if (!isWon) {
+            currentPlayer.changePlayer();
+          }
         }
       })
-      
-    }
-  }
-
-  const checkMatchThree = () => {
-    if (
-      ( (boxArr[0][0].style.backgroundColor == 'red') && (boxArr[0][1].style.backgroundColor == 'red') && (boxArr[0][2].style.backgroundColor == 'red') ) ||
-      ( (boxArr[1][0].style.backgroundColor == 'red') && (boxArr[1][1].style.backgroundColor == 'red') && (boxArr[1][2].style.backgroundColor == 'red') ) ||
-      ( (boxArr[2][0].style.backgroundColor == 'red') && (boxArr[2][1].style.backgroundColor == 'red') && (boxArr[2][2].style.backgroundColor == 'red') ) ||
-      ( (boxArr[0][0].style.backgroundColor == 'red') && (boxArr[1][1].style.backgroundColor == 'red') && (boxArr[2][2].style.backgroundColor == 'red') ) ||
-      ( (boxArr[2][0].style.backgroundColor == 'red') && (boxArr[1][1].style.backgroundColor == 'red') && (boxArr[0][2].style.backgroundColor == 'red') ) ||
-      ( (boxArr[0][0].style.backgroundColor == 'red') && (boxArr[1][0].style.backgroundColor == 'red') && (boxArr[2][0].style.backgroundColor == 'red') ) ||
-      ( (boxArr[0][1].style.backgroundColor == 'red') && (boxArr[1][1].style.backgroundColor == 'red') && (boxArr[2][1].style.backgroundColor == 'red') ) ||
-      ( (boxArr[0][2].style.backgroundColor == 'red') && (boxArr[1][2].style.backgroundColor == 'red') && (boxArr[2][2].style.backgroundColor == 'red') )
-    ){
-      document.getElementById(`result`).innerHTML = `That's a match three! ${play1} won!`;
-      isWon = true;
     }
   }
 
